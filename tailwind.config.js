0975a170043b0460da57f17ee903a041b7f3b587
Gaurgle <justwannagroove@gmail.com>
2025-04-13 12:37:57 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class', // or 'media' for media query based
+    content: [
+        "./src/**/*.{html,js}", // adjust based on your project structure
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
